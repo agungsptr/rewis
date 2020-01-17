838affb29665d37b0518e14f29e1a436ef3c67bf
agungsptr <agung.eka1999@gmail.com>
2020-01-17 06:14:24 +0000

    coba

--- index.php
+++ index.php
@@ -46,55 +46,9 @@
             </form>
         </div>
     </div>
-    <div class="row ml-2">
-        <?php
-        require('model/TempatWisata.php');
-        $tempatWisata = new TempatWisata;
-        if (isset($_GET['search'])) {
-            $items = $tempatWisata->like('nama_tempat', $_GET['search']);
-        } else {
-            $items = $tempatWisata->all();
-        }
-        $id = mysqli_fetch_assoc($items);
 
-        foreach ($items as $item) {
-        ?>
+    
 
-            <div class="col">
-                <div class="card shadow mb-4" style="width: 20rem;">
-                    <div class="inner">
-                        <img class="card-img-top" src="img/img-sample.jpg" alt="img">
-                    </div>
-
-                    <div class="card-body text-center">
-                        <h5 class="card-title"><?= $item['nama_tempat'] ?></h5>
-                        <p class="cardy">
-                            <?= $item['deskripsi']?>
-                        </p>
-                        <div class="mb-3 text-left">
-                            <div class="row">
-                                <div class="col-5">Kabupaten</div>
-                                <div class="col-7"><?= $item['kab'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-5">Alamat</div>
-                                <div class="col-7 cardy"><?= $item['alamat'] ?></div>
-                            </div>
-                            <div class="row">
-                                <div class="col-5">Biaya</div>
-                                <div class="col-7">Rp. <?= $item['biaya'] ?></div>
-                            </div>
-                        </div>
-                        <a href="/rewis/view/detail/?id=<?= $item['id']?>" class="btn btn-success pl-4 pr-4">Detail</a>
-                    </div>
-                </div>
-            </div>
-
-        <?php
-        }
-        ?>
-
-    </div>
 </div>
 
 <?php include('view/layouts/footer.php') ?>
